2c7a0f1c112260843e46e28410e64547a1792dd3
Stefan Mathis <stefan_mathis@web.de>
2025-10-24 18:34:11 +0200

    stored build.rs

--- build.rs
+++ build.rs
@@ -5,17 +5,17 @@ This build script makes sure that images are sourced locally when building the
 docs locally and from Github when building the images on docs.rs
  */
 fn main() {
-    // let readme = fs::read_to_string("README.md").unwrap();
+    let readme = fs::read_to_string("README.md").unwrap();
 
-    // let processed = if env::var("DOCS_RS").is_ok() {
-    //     readme.replace(
-    //         "](docs/",
-    //         "](https://raw.githubusercontent.com/StefanMathis/akima_spline/main/docs/",
-    //     )
-    // } else {
-    //     readme
-    // };
+    let processed = if env::var("DOCS_RS").is_ok() {
+        readme.replace(
+            "](docs/",
+            "](https://raw.githubusercontent.com/StefanMathis/akima_spline/main/docs/",
+        )
+    } else {
+        readme
+    };
 
-    // fs::write("target/README_FOR_DOCS.md", processed).unwrap();
-    // println!("cargo:rustc-env=README_PATH=target/README_FOR_DOCS.md");
+    fs::write("target/README_FOR_DOCS.md", processed).unwrap();
+    println!("cargo:rustc-env=README_PATH=target/README_FOR_DOCS.md");
 }
